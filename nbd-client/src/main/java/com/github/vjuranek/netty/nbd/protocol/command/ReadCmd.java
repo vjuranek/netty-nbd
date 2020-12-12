d3b3c4f7da18646267d40c326d6764650f3532c8
Vojtech Juranek <vojtech.juranek@gmail.com>
2020-12-12 22:08:52 +0100

    commands: add read command

---
+++ nbd-client/src/main/java/com/github/vjuranek/netty/nbd/protocol/command/ReadCmd.java
@@ -0,0 +1,28 @@
+package com.github.vjuranek.netty.nbd.protocol.command;
+
+import com.github.vjuranek.netty.nbd.protocol.Constants;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Read command (NBD_CMD_READ).
+ *
+ * @author vjuranek
+ * @see <a href="https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md#request-types">NBD_CMD_READ</a>
+ */
+public class ReadCmd extends NbdCmd {
+
+    private static AtomicInteger handle = new AtomicInteger(0);
+
+    public static int nextHandle() {
+        return handle.incrementAndGet();
+    }
+
+    public ReadCmd(final long offset, final int length) {
+        super((short)0, Constants.NBD_CMD_READ, nextHandle(), offset, length);
+    }
+
+    public ReadCmd(final short flags, final long handle, final long offset, final int length) {
+        super(flags, Constants.NBD_CMD_READ, handle, offset, length);
+    }
+}
