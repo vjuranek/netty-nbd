20bc78e78ff900775bc2cc747ba131b2ef6ae59f
Vojtech Juranek <vojtech.juranek@gmail.com>
2020-12-22 22:35:01 +0100

    More GoOptionHandler tests

--- nbd-client/src/test/java/com/github/vjuranek/netty/nbd/client/option/GoHandlerTest.java
+++ nbd-client/src/test/java/com/github/vjuranek/netty/nbd/client/option/GoHandlerTest.java
@@ -30,4 +30,71 @@ public class GoHandlerTest {
         assertEquals(0, goHandler.reply.size());
     }
 
+    @Test
+    public void testNbdInfoBlockSize() throws InterruptedException {
+        GoHandler goHandler = new GoHandler();
+        EmbeddedChannel channel = new EmbeddedChannel(goHandler);
+
+        ByteBuf buf = Unpooled.buffer(20);
+        buf.writeLong(Constants.NBD_STRUCTURED_REPLY_MAGIC);
+        buf.writeInt(Constants.NBD_OPT_STRUCTURED_REPLY);
+        buf.writeInt(Constants.NBD_REP_ACK);
+        buf.writeInt(Constants.INFO_EXPORT_REPLY_LENGTH);
+        buf.writeShort(Constants.NBD_INFO_EXPORT);
+        buf.writeInt(512);
+        buf.writeInt(512);
+        buf.writeInt(512);
+
+        channel.writeInbound(buf);
+
+        int rc = Unpooled.buffer(4).writeBytes(goHandler.reply.take()).readInt();
+        assertEquals(Constants.NBD_REP_ACK, rc);
+        assertEquals(0, goHandler.reply.size());
+    }
+
+    @Test
+    public void testNbdInfoExport() throws InterruptedException {
+        GoHandler goHandler = new GoHandler();
+        EmbeddedChannel channel = new EmbeddedChannel(goHandler);
+
+        ByteBuf buf = Unpooled.buffer(20);
+        buf.writeLong(Constants.NBD_STRUCTURED_REPLY_MAGIC);
+        buf.writeInt(Constants.NBD_OPT_STRUCTURED_REPLY);
+        buf.writeInt(Constants.NBD_REP_ACK);
+        buf.writeInt(Constants.INFO_EXPORT_REPLY_LENGTH);
+        buf.writeShort(Constants.NBD_INFO_EXPORT);
+        buf.writeLong(10L);
+        buf.writeShort(0);
+
+        channel.writeInbound(buf);
+
+        int rc = Unpooled.buffer(4).writeBytes(goHandler.reply.take()).readInt();
+        assertEquals(Constants.NBD_REP_ACK, rc);
+        assertEquals(0, goHandler.reply.size());
+    }
+
+    @Test
+    public void testNbdInfoExportWrongSize() throws InterruptedException {
+        GoHandler goHandler = new GoHandler();
+        EmbeddedChannel channel = new EmbeddedChannel(goHandler);
+
+        ByteBuf buf = Unpooled.buffer(20);
+        buf.writeLong(Constants.NBD_STRUCTURED_REPLY_MAGIC);
+        buf.writeInt(Constants.NBD_OPT_STRUCTURED_REPLY);
+        buf.writeInt(Constants.NBD_REP_ACK);
+        buf.writeInt(Constants.INFO_EXPORT_REPLY_LENGTH - 1);
+        buf.writeShort(Constants.NBD_INFO_EXPORT);
+        buf.writeLong(10L);
+        buf.writeShort(0);
+
+        try {
+            channel.writeInbound(buf);
+            // TODO: wait for a short time? The calls are async.
+            throw new AssertionError("Handler should throw an exception");
+        } catch (IllegalStateException e) {
+            assertEquals("Size of NBD_INFO_EXPORT has to be 12, but got 11", e.getMessage());
+        }
+
+    }
+
 }
